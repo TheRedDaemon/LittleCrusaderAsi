15336f00d58dab4a8a981e2a8868691b95558436
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2020-10-30 01:52:43 +0100

    [DIRECT_DRAW] clean up, also test with potential menu windows

--- src/LittleCrusaderAsi/modclasses/bltOverlay.cpp
+++ src/LittleCrusaderAsi/modclasses/bltOverlay.cpp
@@ -1,147 +1,42 @@
 
 #include "bltOverlay.h"
 
-#include <initguid.h>
-#include "ddraw.h"
-
 // main source ddraw: https://www.codeproject.com/Articles/2370/Introduction-to-DirectDraw-and-Surface-Blitting
 // another: http://www.visualbasicworld.de/tutorial-directx-directdraw.html
 namespace modclasses
 {
-  static IDirectDraw7* dd7InterfacePtr;
-  static IDirectDrawSurface7* dd7SurfacePtr;
-  static IDirectDrawSurface7* dd7BackbufferPtr;
+  // amcro found online, source: https://gamedev.net/forums/topic/289485-16-bit-color-on-directdraw-surfaces/2825618/
+  #define RGB16(r,g,b) ((b>>3)|((g>>2)<<5)|((r>>3)<<11))
 
-  // this might need far more testing -> dd to d9 might create far other issues
-  static bool doNotKeepDD7Interface = true; // for d7tod9 -> removing the references removes handling window
-  // in case of non compatible mode Crusader (XP), VideoMemory creates errors
-  // if true, there will be no test performed again
-  static bool disableHardwareTest = false;
-
-  // test
-  static IDirectDrawSurface7* offscreenSurf;
+  // pointer for static functions
+  static BltOverlay *overPtr{ nullptr };
   
+
   // NOTE: -> linking to static functions seem to work pretty good -> the copy needs to be nice though (in first tests, _stdcall was missing)
   // maybe there are some stuff like _stdcall etc that might help me?
 
 
-  // IDirectDraw7 ptr is invalid once it reaches this position
-  // -> old surfaces sometimes are lost, sometimes they work
-  // -> depends on asiLoader (and its config)
-  // test with different main dd7 object -> did not get it to work
-  // it returned ok, but nothing was drawn on screen
-  static HRESULT _stdcall CreateSurfaceFake(IDirectDraw7* const that, LPDDSURFACEDESC2 desc2, LPDIRECTDRAWSURFACE7* surf7, IUnknown* unkn)
+  HRESULT _stdcall BltOverlay::CreateSurfaceFake(IDirectDraw7* const that, LPDDSURFACEDESC2 desc2, LPDIRECTDRAWSURFACE7* surf7, IUnknown* unkn)
   {
     // keep control over pointer
-    if (dd7InterfacePtr != nullptr)
-    {
-      offscreenSurf->Release();
-
-      if (!doNotKeepDD7Interface)
-      {
-        dd7InterfacePtr->Release();
-      }
-    }
+    overPtr->releaseDDInterfaces();
 
     HRESULT frontSurfRes{ that->CreateSurface(desc2, surf7, unkn) };
 
     if (frontSurfRes == S_OK)
     {
-      HRESULT res; // need to clean up
-
-      dd7InterfacePtr = that;
-      if (!doNotKeepDD7Interface)
-      {
-        dd7InterfacePtr->AddRef();
-      }
-
-      dd7SurfacePtr = *surf7;
-
-      DDSCAPS2 ddscapsBack;
-      ZeroMemory(&ddscapsBack, sizeof(ddscapsBack));
-      ddscapsBack.dwCaps = DDSCAPS_BACKBUFFER;
-      res = dd7SurfacePtr->GetAttachedSurface(&ddscapsBack, &dd7BackbufferPtr);
-      if (res == DD_OK)
-      {
-        LOG(INFO) << "Got backbuffer.";
-      }
-
-      //dd7SurfacePtr->Flip(NULL, 0); // asi + 2E701C
-      // wrapper+BB6F0
-      // dxwrapper.dll+AAAF0 (flip function)
-
-      // write to buffer test, create surface:
-
-      DDSURFACEDESC2 offscreenSurfDes;
-      ZeroMemory(&offscreenSurfDes, sizeof(offscreenSurfDes));
-
-      offscreenSurfDes.dwSize = sizeof(offscreenSurfDes);
-      offscreenSurfDes.dwFlags = DDSD_CAPS | DDSD_WIDTH | DDSD_HEIGHT;
-      
-      // important: without compatibility mode, stronghold does not support Hardware rendering -> DDSCAPS_VIDEOMEMORY only in this case
-      // -> could be done by flag in config -> this might be the case for other flags
-      offscreenSurfDes.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN;
-      if (!disableHardwareTest)
-      {
-        offscreenSurfDes.ddsCaps.dwCaps |= DDSCAPS_VIDEOMEMORY;
-      }
-
-      offscreenSurfDes.dwWidth = 100;
-      offscreenSurfDes.dwHeight = 100;
-
-      res = dd7InterfacePtr->CreateSurface(&offscreenSurfDes, &offscreenSurf, NULL);
-      if (res == DDERR_NODIRECTDRAWHW)
-      {
-        offscreenSurfDes.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN;
-        res = dd7InterfacePtr->CreateSurface(&offscreenSurfDes, &offscreenSurf, NULL);
-        
-        disableHardwareTest = true;
-        LOG(WARNING) << "No hardware support found for DirectDraw. Try compatibility mode for WinXP.";
-        if (res != S_OK)
-        {
-          LOG(ERROR) << "Failed to create offscreen surface.";
-        }
-      }
-
-
-      LOG(INFO) << "Got first create.";
+      overPtr->prepareDDInterfaces(that, *surf7);
     }
 
     return frontSurfRes;
   }
 
 
-  static int testAdd{ 0 };
-
-
-  // position flip: 0047064D (not easily redirectable)
-  // maybe here: 00470645 write address of flip function into edx
-  // flip is called at a relative constant time -> no menu responsiveness issues
-  static HRESULT _stdcall FlipFake(IDirectDrawSurface7* const that, LPDIRECTDRAWSURFACE7 surf7, DWORD flags)
+  HRESULT _stdcall BltOverlay::FlipFake(IDirectDrawSurface7* const that, LPDIRECTDRAWSURFACE7 surf7, DWORD flags)
   {
-    RECT rect;
-    rect.left = 0;
-    rect.top = 0;
-    rect.right = 100 + 0; // seems to use exclusive
-    rect.bottom = 100 + 0;
+    overPtr->bltMainDDOffSurf();
 
-    HRESULT bltRes = dd7BackbufferPtr->BltFast(300 + testAdd, 300, offscreenSurf, &rect, DDBLTFAST_NOCOLORKEY);
-    ++testAdd;
-    if (testAdd > 100)
-    {
-      testAdd = 0;
-    }
-
-    HRESULT res{ that->Flip(surf7, flags) };
-    if (res == S_OK)
-    {
-      // LOG(INFO) << "FLIP."; // <--- I hope this spams -> it spams
-      if (that != dd7SurfacePtr)
-      {
-        LOG(INFO) << "surface changed"; // if the surface is the same, so will be the ddObject...I hope
-      }
-    }
-    return res;
+    return that->Flip(surf7, flags);
   }
 
 
@@ -152,38 +47,52 @@ namespace modclasses
 
   BltOverlay::BltOverlay(const std::weak_ptr<modcore::ModKeeper> modKeeper, const Json &config) : ModBase(modKeeper)
   {
+    auto confIt = config.find("doNotKeepDD7Interface");
+    if (confIt != config.end() && confIt.value().is_boolean())
+    {
+      doNotKeepDD7Interface = confIt.value().get<bool>();
+    }
+    else
+    {
+      LOG(WARNING) << "BltOverlay: No valid 'doNotKeepDD7Interface' found. Needs to be boolean. Defaults to 'false'. "
+        << "Only used for some D7to9 modes (if asiloader is dxwrapper) to prevent issues. Ex. window modes needs 'true', or it vanishes.";
+    }
   }
 
   void BltOverlay::initialize()
   {
-    auto addressResolver = getMod<AddressResolver>();
+    bool issue{ false };
 
-    if (addressResolver)
+    if (overPtr)
     {
-      // request addresses
-      if (addressResolver->requestAddresses(usedAddresses, *this))
-      {
-        // TODO(?): maybe some byte writer would be an idea... (but assembly to opcode would be too much/difficult I guess)
-        // would need to construct byte array out of given values with types...
+      issue = true;
+      LOG(ERROR) << "BltOverlay needs to be singleton. Creating another BltOverlay is not allowed.";
+    }
+    overPtr = this;
+    
+    auto addressResolver = getMod<AddressResolver>();
+    if (!issue && addressResolver && addressResolver->requestAddresses(usedAddresses, *this))
+    {
+      // TODO(?): maybe some byte writer would be an idea... (but assembly to opcode would be too much/difficult I guess)
+      // would need to construct byte array out of given values with types...
 
-        // first CreateSurface -> responsible for the main window, called when the app started, switched back to or the resolution changed
-        unsigned char* surfCall{ addressResolver->getAddressPointer<unsigned char>(Address::DD_MainSurfaceCreate, *this) };
-        int32_t* surfAddr{ reinterpret_cast<int32_t*>(surfCall + 1) };
-        
-        // call rel32 -> rel32 = absolute pointer - pos from where i write to - length of edited segment (or absolute pointer - next address?)
-        *surfCall = 0xE8;
-        *surfAddr = reinterpret_cast<int32_t>(CreateSurfaceFake) - reinterpret_cast<int32_t>(surfCall) - 5;
+      // first CreateSurface -> responsible for the main window, called when the app started, switched back to or the resolution changed
+      unsigned char* surfCall{ addressResolver->getAddressPointer<unsigned char>(Address::DD_MainSurfaceCreate, *this) };
+      int32_t* surfAddr{ reinterpret_cast<int32_t*>(surfCall + 1) };
 
-        // first found Flip
-        unsigned char* flipMov = addressResolver->getAddressPointer<unsigned char>(Address::DD_MainFlip, *this);
-        int32_t* flipAddr{ reinterpret_cast<int32_t*>(flipMov + 1) };
-        
-        // mov edx, func address
-        *flipMov = 0xBA;
-        *flipAddr = reinterpret_cast<int>(FlipFake);
+      // call rel32 -> rel32 = absolute pointer - pos from where i write to - length of edited segment (or absolute pointer - next address?)
+      *surfCall = 0xE8;
+      *surfAddr = reinterpret_cast<int32_t>(CreateSurfaceFake) - reinterpret_cast<int32_t>(surfCall) - 5;
 
-        initialized = true;
-      }
+      // first found Flip
+      unsigned char* flipMov = addressResolver->getAddressPointer<unsigned char>(Address::DD_MainFlip, *this);
+      int32_t* flipAddr{ reinterpret_cast<int32_t*>(flipMov + 1) };
+
+      // mov edx, func address
+      *flipMov = 0xBA;
+      *flipAddr = reinterpret_cast<int>(FlipFake);
+
+      initialized = true;
     }
 
     initialized ? LOG(INFO) << "BltOverlay initialized." : LOG(WARNING) << "BltOverlay was not initialized.";
@@ -194,6 +103,144 @@ namespace modclasses
   {
     // no resource delete -> process end should free it anyway (it also prevents problems from not knowing if still valid)
     // offscreenSurf->Release();
+  }
+
+
+  // intern functions //
+
+
+  void BltOverlay::createOffSurface(IDirectDrawSurface7** surf, DWORD width, DWORD height, DWORD fillColor)
+  {
+    DDSURFACEDESC2 offscreenSurfDes;
+    zeroDDObjectAndSetSize<DDSURFACEDESC2>(offscreenSurfDes);
+
+    offscreenSurfDes.dwFlags = DDSD_CAPS | DDSD_WIDTH | DDSD_HEIGHT;
+
+    // important: without compatibility mode, stronghold does not support Hardware rendering -> DDSCAPS_VIDEOMEMORY only in this case
+    // -> could be done by flag in config -> this might be the case for other flags
+    offscreenSurfDes.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN;
+    if (!disableHardwareTest)
+    {
+      offscreenSurfDes.ddsCaps.dwCaps |= DDSCAPS_VIDEOMEMORY;
+    }
+
+    offscreenSurfDes.dwWidth = width;
+    offscreenSurfDes.dwHeight = height;
+
+    // test
+    //offscreenSurfDes.dwFlags |= DDSD_PIXELFORMAT;
+    //zeroDDObjectAndSetSize(offscreenSurfDes.ddpfPixelFormat);
+    //offscreenSurfDes.ddpfPixelFormat.dwFlags = DDPF_ALPHAPIXELS | DDPF_ALPHAPREMULT | DDPF_PALETTEINDEXED8 | DDPF_RGB;
+    //offscreenSurfDes.ddpfPixelFormat.dwRGBBitCount = 32;
+
+    HRESULT res = dd7InterfacePtr->CreateSurface(&offscreenSurfDes, surf, NULL);
+    if (res == DDERR_NODIRECTDRAWHW)
+    {
+      offscreenSurfDes.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN;
+      res = dd7InterfacePtr->CreateSurface(&offscreenSurfDes, surf, NULL);
+
+      disableHardwareTest = true;
+      LOG(WARNING) << "No hardware support found for DirectDraw. Try compatibility mode for WinXP.";
+    }
+
+    if (res != S_OK)
+    {
+      LOG(ERROR) << "Failed to create offscreen surface.";
+      return;
+    }
+
+    // dummy initial color fill
+    DDBLTFX fx;
+    zeroDDObjectAndSetSize<DDBLTFX>(fx);
+
+    fx.dwFillColor = fillColor;
+
+    res = (*surf)->Blt(NULL, NULL, NULL, DDBLT_COLORFILL, &fx);
+  }
+
+
+  void BltOverlay::releaseDDInterfaces()
+  {
+    if (dd7InterfacePtr != nullptr)
+    {
+      menuOffSurf->Release();
+      textOffSurf->Release();
+      inputOffSurf->Release();
+
+      if (!doNotKeepDD7Interface)
+      {
+        dd7InterfacePtr->Release();
+      }
+    }
+  }
+
+
+  void BltOverlay::prepareDDInterfaces(IDirectDraw7* const that, IDirectDrawSurface7* const dd7SurfacePtr)
+  {
+    HRESULT res; // need to clean up
+
+    dd7InterfacePtr = that;
+    if (!doNotKeepDD7Interface)
+    {
+      dd7InterfacePtr->AddRef();
+    }
+
+    DDSCAPS2 ddscapsBack;
+    zeroDDObject<DDSCAPS2>(ddscapsBack);
+    ddscapsBack.dwCaps = DDSCAPS_BACKBUFFER;
+    res = dd7SurfacePtr->GetAttachedSurface(&ddscapsBack, &dd7BackbufferPtr);
+    if (res == DD_OK)
+    {
+      LOG(INFO) << "Got backbuffer.";
+    }
+
+    createOffSurface(&menuOffSurf, menuRect.right, menuRect.bottom, RGB16(0,0,255));
+    createOffSurface(&textOffSurf, textRect.right, textRect.bottom, RGB16(255, 0, 0));
+    createOffSurface(&inputOffSurf, inputRect.right, inputRect.bottom, RGB16(0, 255, 0));
+    
+    // set input pos to middle
+    DDSURFACEDESC2 surfaceInfos;
+    zeroDDObjectAndSetSize<DDSURFACEDESC2>(surfaceInfos);
+    dd7SurfacePtr->GetSurfaceDesc(&surfaceInfos);
+
+    /*
+    DDPIXELFORMAT px;
+    zeroDDObjectAndSetSize(px);
+    menuOffSurf->GetPixelFormat(&px);
+    dd7SurfacePtr->GetPixelFormat(&px);
+    */
+
+    inputPos.first = surfaceInfos.dwWidth / 2 - inputRect.right / 2;
+    inputPos.second = surfaceInfos.dwHeight / 2 - inputRect.bottom / 2;
+
+    LOG(INFO) << "Finished surface prepare.";
+  }
+
+
+  void BltOverlay::bltMainDDOffSurf()
+  {
+    /*
+    // test
+    static int testAdd{ 0 };
+
+    RECT rect;
+    rect.left = 0;
+    rect.top = 0;
+    rect.right = 100 + 0; // seems to use exclusive
+    rect.bottom = 100 + 0;
+
+    HRESULT bltRes = dd7BackbufferPtr->BltFast(300 + testAdd, 300, mainOffSurf, &rect, DDBLTFAST_NOCOLORKEY);
+    ++testAdd;
+    if (testAdd > 100)
+    {
+      testAdd = 0;
+    }
+    */
+
+    // NOTE: normal blt can auto transform the size to fit
+    dd7BackbufferPtr->BltFast(menuPos.first, menuPos.second, menuOffSurf, &menuRect, DDBLTFAST_NOCOLORKEY);
+    dd7BackbufferPtr->BltFast(textPos.first, textPos.second, textOffSurf, &textRect, DDBLTFAST_NOCOLORKEY);
+    dd7BackbufferPtr->BltFast(inputPos.first, inputPos.second, inputOffSurf, &inputRect, DDBLTFAST_NOCOLORKEY);
   }
 
 
--- src/LittleCrusaderAsi/modclasses/bltOverlay.h
+++ src/LittleCrusaderAsi/modclasses/bltOverlay.h
@@ -6,6 +6,9 @@
 #include "keyboardInterceptor.h"
 #include "addressResolver.h"
 
+// DirectDraw
+#include "ddraw.h"
+
 namespace modclasses
 {
 
@@ -13,6 +16,37 @@ namespace modclasses
   class BltOverlay : public ModBase
   {
   private:
+
+    IDirectDraw7* dd7InterfacePtr{ nullptr };            // currentMainInterface
+    IDirectDrawSurface7* dd7BackbufferPtr{ nullptr };    // currentBackbufferInterface
+
+    // keeps ddInterfaceRefs
+    // -> for d7tod9 (removing the references removes handling window for ex.)
+    bool doNotKeepDD7Interface{ false };
+    
+    // DDs does not have hardware support out of the box
+    // -> if calls fail, this will be set to true, preventing repeats
+    bool disableHardwareTest{ false };
+
+    // -> redesign in your head:
+    // alpha blending seems to be a complicated thing 
+    // -> there is no function support, one would have to do it manually in software
+    // -> so plan changed -> text stuff smaller and everything opaque menu
+
+    // main surfaces applied per fast blt
+    IDirectDrawSurface7* menuOffSurf; // menu
+    IDirectDrawSurface7* textOffSurf; // display text
+    IDirectDrawSurface7* inputOffSurf; // input field in the middle of screen
+    // and the rect structures (contain also size for them) (hardcoded currently
+    RECT menuRect{0, 0, 300, 500 };
+    RECT textRect{ 0, 0, 500, 250 };
+    RECT inputRect{ 0, 0, 300, 100 };
+    // and positions on screen
+    std::pair<DWORD, DWORD> menuPos{ 0, 0 };  // upper corner
+    std::pair<DWORD, DWORD> textPos{ 300, 0 };        // next to menu
+    std::pair<DWORD, DWORD> inputPos{ 0, 0 };   // set by program
+
+    // place for whatever structure will be used for additional input stuff
 
     // needed to give the address resolver the right infos
     // can be static, I don't assume changes
@@ -47,6 +81,46 @@ namespace modclasses
 
     // should be defined in .cpp
     void initialize() override;
+
+    // releases current interfaces if new encountered
+    void releaseDDInterfaces();
+
+    // creates all needed interfaces after a new main object was registered
+    void prepareDDInterfaces(IDirectDraw7* const that, IDirectDrawSurface7* const dd7SurfacePtr);
+
+    // blts the overlay surface in the backbuffer
+    void bltMainDDOffSurf();
+
+    // needs pointer to value
+    void createOffSurface(IDirectDrawSurface7** surf, DWORD width, DWORD height, DWORD fillColor);
+
+
+    // static functions: //
+
+    // small helper method to zero ddObject (and set size value)
+    template <typename T>
+    void zeroDDObject(T &ddObject)
+    {
+      // zero memory can be optimized away -> should there every be bugs, use SecureZeroMemory
+      ZeroMemory(&ddObject, sizeof(ddObject));
+    }
+    template <typename T>
+    void zeroDDObjectAndSetSize(T &ddObject)
+    {
+      // zero memory can be optimized away -> should there every be bugs, use SecureZeroMemory
+      ZeroMemory(&ddObject, sizeof(ddObject));
+      ddObject.dwSize = sizeof(ddObject);
+    }
+
+    // IDirectDraw7 ptr is invalid once it reaches this position
+    // -> old surfaces sometimes are lost, sometimes they work
+    // -> depends on asiLoader (and its config)
+    // test with different main dd7 object -> did not get it to work
+    // it returned ok, but nothing was drawn on screen
+    static HRESULT _stdcall CreateSurfaceFake(IDirectDraw7* const that, LPDDSURFACEDESC2 desc2, LPDIRECTDRAWSURFACE7* surf7, IUnknown* unkn);
+
+    // flip is called at a relative constant time -> no menu responsiveness issues
+    static HRESULT _stdcall FlipFake(IDirectDrawSurface7* const that, LPDIRECTDRAWSURFACE7 surf7, DWORD flags);
   };
 }
 
